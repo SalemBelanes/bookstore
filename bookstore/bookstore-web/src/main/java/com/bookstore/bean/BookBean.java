1839be83229bb040e7c5c6eb14e35f0c8f497967
seifeddine_dridi <seifeddine.dridi89@gmail.com>
2015-12-25 08:36:54 +0100

    Added logout feature

--- bookstore/bookstore-web/src/main/java/com/bookstore/bean/BookBean.java
+++ bookstore/bookstore-web/src/main/java/com/bookstore/bean/BookBean.java
@@ -3,8 +3,11 @@ package com.bookstore.bean;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.ejb.Init;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import javax.faces.context.FacesContext;
 
 import com.bookstore.domain.Book;
 import com.bookstore.services.BookRemoteService;
@@ -13,9 +16,18 @@ import com.bookstore.services.BookRemoteService;
 @ViewScoped
 public class BookBean {
 
+	@ManagedProperty(value = "#{userBean}")
+	private UserBean userBean;
 	@EJB
 	private BookRemoteService bookService;
 	private List<Book> bookList;
+
+	public String init() {
+		if (FacesContext.getCurrentInstance().getExternalContext().getSessionMap().get("user") == null) {
+			return "/views/login?faces-redirect=true";
+		}
+		return null;
+	}
 
 	public List<Book> getBookList() {
 		return (bookList = bookService.findAll());
@@ -23,6 +35,14 @@ public class BookBean {
 
 	public void setBookList(List<Book> bookList) {
 		this.bookList = bookList;
+	}
+
+	public UserBean getUserBean() {
+		return userBean;
+	}
+
+	public void setUserBean(UserBean userBean) {
+		this.userBean = userBean;
 	}
 
 }
--- bookstore/bookstore-web/src/main/java/com/bookstore/bean/UserBean.java
+++ bookstore/bookstore-web/src/main/java/com/bookstore/bean/UserBean.java
@@ -1,8 +1,10 @@
 package com.bookstore.bean;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
 
 import com.bookstore.domain.User;
 import com.bookstore.services.UserRemoteService;
@@ -14,6 +16,7 @@ public class UserBean {
 	@EJB
 	private UserRemoteService userService;
 	private User currentUser = new User();
+	private String errorMessage;
 
 	public User getCurrentUser() {
 		return currentUser;
@@ -22,10 +25,24 @@ public class UserBean {
 	public String authenticate() {
 		User user = userService.findUser(currentUser.getEmail(), currentUser.getPassword());
 		if (user == null) {
+			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage(FacesMessage.SEVERITY_ERROR,
+					"Incorrect Email or Passowrd", "Incorrect Email or Passowrd"));
 			return "/views/login";
 		}
+		errorMessage = "";
 		currentUser = user;
+		FacesContext.getCurrentInstance().getExternalContext().getSessionMap().put("user", currentUser);
 		return "/views/displayAllBooks";
+	}
+
+	public String logOut() {
+		FacesContext.getCurrentInstance().getExternalContext().invalidateSession();
+		currentUser = new User();
+		return "/views/login?faces-redirect=true";
+	}
+
+	public String getErrorMessage() {
+		return errorMessage;
 	}
 
 }
